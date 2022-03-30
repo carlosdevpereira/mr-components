e868a8d37359615d08cb5828d6c79e6820c73726
carlosdevpereira <carlos.dev.pereira@gmail.com>
2022-03-30 13:11:56 +0000

    Deploying to docs-deployment from @ carlosdevpereira/mr-components@cde052e3f534677c7e360c9e1badfd79b62815d5 🚀

---
+++ assets/spinner.html.a876b170.js
@@ -0,0 +1,1 @@
+const t={key:"v-09d99da0",path:"/components/spinner.html",title:"Spinner",lang:"en-US",frontmatter:{},excerpt:"",headers:[],git:{updatedTime:1648645856e3,contributors:[{name:"github-actions[bot]",email:"41898282+github-actions[bot]@users.noreply.github.com",commits:1}]},filePathRelative:"components/spinner.md"};export{t as data};
