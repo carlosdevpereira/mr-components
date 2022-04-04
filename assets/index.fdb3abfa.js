b63bccda28c5d87885af5674f8494ee0b421e66a
carlosdevpereira <carlos.dev.pereira@gmail.com>
2022-04-04 18:53:25 +0000

    Deploying to docs-deployment from @ carlosdevpereira/mr-components@ea794ef696065ded27d00674a50fe4aee24a031c 🚀

--- assets/index.fdb3abfa.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as _,a as n}from"./app.84fd00b4.js";const s={},r={class:"mr-spinner"};function c(a,t){return _(),n("span",r)}var d=e(s,[["render",c],["__file","index.vue"]]);export{d as default};
