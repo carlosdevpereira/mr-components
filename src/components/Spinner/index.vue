c44bcae17d615893ff565faaa08a0221b775944f
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-04-05 20:44:18 +0100

    fix: spinner component animation (#77)

--- src/components/Spinner/index.vue
+++ src/components/Spinner/index.vue
@@ -1,5 +1,12 @@
 <template>
-	<span class="mr-spinner" />
+	<div class="mr-spinner">
+		<div
+			v-for="i in 2"
+			:key="i"
+			class="mr-spinner-item"
+			:class="`mr-spinner-item-${i}`"
+		/>
+	</div>
 </template>
 
 <style lang="scss" src="./index.scss" />
