4f8beda714a55a4864dea3d34e332652301557e6
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-05-03 22:50:22 +0100

    fix: table numeric filters (#146)

--- src/utils/local-filtering.js
+++ src/utils/local-filtering.js
@@ -11,9 +11,9 @@ export default {
 				if (!filter.value) continue
 
 				if (filter.operator.key === 'eq') {
-					isValid = row[filter.column.key] === filter.value
+					isValid = `${row[filter.column.key]}` === `${filter.value}`
 				} else if (filter.operator.key === 'df') {
-					isValid = row[filter.column.key] !== filter.value
+					isValid = `${row[filter.column.key]}` !== `${filter.value}`
 				} else if (filter.operator.key === 'ut') {
 					isValid = row[filter.column.key] > filter.value
 				} else if (filter.operator.key === 'lt') {
@@ -23,23 +23,23 @@ export default {
 				} else if (filter.operator.key === 'let') {
 					isValid = row[filter.column.key] <= filter.value
 				} else if (filter.operator.key === 'contains') {
-					isValid = row[filter.column.key].includes(filter.value)
+					isValid = `${row[filter.column.key]}`.includes(`${filter.value}`)
 				} else if (filter.operator.key === 'icontains') {
-					isValid = row[filter.column.key]
+					isValid = `${row[filter.column.key]}`
 						.toUpperCase()
-						.includes(filter.value.toUpperCase())
+						.includes(`${filter.value}`.toUpperCase())
 				} else if (filter.operator.key === 'starts') {
-					isValid = row[filter.column.key].startsWith(filter.value)
+					isValid = `${row[filter.column.key]}`.startsWith(`${filter.value}`)
 				} else if (filter.operator.key === 'istarts') {
-					isValid = row[filter.column.key]
+					isValid = `${row[filter.column.key]}`
 						.toUpperCase()
-						.startsWith(filter.value.toUpperCase())
+						.startsWith(`${filter.value}`.toUpperCase())
 				} else if (filter.operator.key === 'ends') {
-					isValid = row[filter.column.key].endsWith(filter.value)
+					isValid = `${row[filter.column.key]}`.endsWith(`${filter.value}`)
 				} else if (filter.operator.key === 'iends') {
-					isValid = row[filter.column.key]
+					isValid = `${row[filter.column.key]}`
 						.toUpperCase()
-						.endsWith(filter.value.toUpperCase())
+						.endsWith(`${filter.value}`.toUpperCase())
 				}
 
 				if (isValid === false) break
