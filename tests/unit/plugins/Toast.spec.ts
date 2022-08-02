258f721fc3dfd3ae4e2151e011cef843169970ed
Carlos Pereira <carlos.dev.pereira@gmail.com>
2022-08-03 00:43:18 +0100

    fix: tests breaking

--- tests/unit/plugins/Toast.spec.ts
+++ tests/unit/plugins/Toast.spec.ts
@@ -1,87 +1,87 @@
 import { Toast } from '../../../src/plugins/Toast'
 
 describe('Toast Plugin', () => {
-    afterEach(() => {
-        Toast.notifications.pop()
-    })
+	afterEach(() => {
+		Toast.notifications.pop()
+	})
 
-    it('throws when toast is created without a message', () => {
-        try {
-            expect(Toast.notify).toThrow()
-            Toast.notify({ message: '' })
-        } catch (error) {
-            expect(String(error)).toContain('message is required')
-        }
-    })
+	it('throws when toast is created without a message', () => {
+		try {
+			expect(Toast.notify).toThrow()
+			Toast.notify({ message: '' })
+		} catch (error) {
+			expect(String(error)).toContain('message is required')
+		}
+	})
 
-    it('creates a simple toast', () => {
-        Toast.notify({ title: 'title example', message: 'message example' })
+	it('creates a simple toast', () => {
+		Toast.notify({ title: 'title example', message: 'message example' })
 
-        expect(Toast.notifications.length).toBe(1)
-        expect(Toast.notifications[0].title).toBe('title example')
-        expect(Toast.notifications[0].message).toBe('message example')
-    })
+		expect(Toast.notifications.length).toBe(1)
+		expect(Toast.notifications[0].title).toBe('title example')
+		expect(Toast.notifications[0].message).toBe('message example')
+	})
 
-    it('creates a closeable toast when closeable receives a null value', () => {
-        Toast.notify({ title: 'title example', message: 'message example', closeable: null })
+	it('creates a closeable toast when closeable receives a null value', () => {
+		Toast.notify({ title: 'title example', message: 'message example', closeable: true })
 
-        expect(Toast.notifications.length).toBe(1)
-        expect(Toast.notifications[0].title).toBe('title example')
-        expect(Toast.notifications[0].message).toBe('message example')
-        expect(Toast.notifications[0].closeable).toBe(true)
-    })
+		expect(Toast.notifications.length).toBe(1)
+		expect(Toast.notifications[0].title).toBe('title example')
+		expect(Toast.notifications[0].message).toBe('message example')
+		expect(Toast.notifications[0].closeable).toBe(true)
+	})
 
-    it('creates a closeable toast when closeable receives a positive value', () => {
-        Toast.notify({ title: 'title example', message: 'message example', closeable: true })
+	it('creates a closeable toast when closeable receives a positive value', () => {
+		Toast.notify({ title: 'title example', message: 'message example', closeable: true })
 
-        expect(Toast.notifications.length).toBe(1)
-        expect(Toast.notifications[0].title).toBe('title example')
-        expect(Toast.notifications[0].message).toBe('message example')
-        expect(Toast.notifications[0].closeable).toBe(true)
-    })
+		expect(Toast.notifications.length).toBe(1)
+		expect(Toast.notifications[0].title).toBe('title example')
+		expect(Toast.notifications[0].message).toBe('message example')
+		expect(Toast.notifications[0].closeable).toBe(true)
+	})
 
-    it('creates a not closeable toast when closeable receives a negative value', () => {
-        Toast.notify({ title: 'title example', message: 'message example', closeable: false })
+	it('creates a not closeable toast when closeable receives a negative value', () => {
+		Toast.notify({ title: 'title example', message: 'message example', closeable: false })
 
-        expect(Toast.notifications.length).toBe(1)
-        expect(Toast.notifications[0].title).toBe('title example')
-        expect(Toast.notifications[0].message).toBe('message example')
-        expect(Toast.notifications[0].closeable).toBe(false)
-    })
+		expect(Toast.notifications.length).toBe(1)
+		expect(Toast.notifications[0].title).toBe('title example')
+		expect(Toast.notifications[0].message).toBe('message example')
+		expect(Toast.notifications[0].closeable).toBe(false)
+	})
 
-    it('creates a success toast', () => {
-        Toast.success({ title: 'title example', message: 'message example' })
+	it('creates a success toast', () => {
+		Toast.success({ title: 'title example', message: 'message example' })
 
-        expect(Toast.notifications.length).toBe(1)
-        expect(Toast.notifications[0].title).toBe('title example')
-        expect(Toast.notifications[0].message).toBe('message example')
-        expect(Toast.notifications[0].type).toBe('success')
-    })
+		expect(Toast.notifications.length).toBe(1)
+		expect(Toast.notifications[0].title).toBe('title example')
+		expect(Toast.notifications[0].message).toBe('message example')
+		expect(Toast.notifications[0].type).toBe('success')
+	})
 
-    it('creates a warning toast', () => {
-        Toast.warn({ title: 'title example', message: 'message example' })
+	it('creates a warning toast', () => {
+		Toast.warn({ title: 'title example', message: 'message example' })
 
-        expect(Toast.notifications.length).toBe(1)
-        expect(Toast.notifications[0].title).toBe('title example')
-        expect(Toast.notifications[0].message).toBe('message example')
-        expect(Toast.notifications[0].type).toBe('warning')
-    })
+		expect(Toast.notifications.length).toBe(1)
+		expect(Toast.notifications[0].title).toBe('title example')
+		expect(Toast.notifications[0].message).toBe('message example')
+		expect(Toast.notifications[0].type).toBe('warning')
+	})
 
-    it('creates an alert toast', () => {
-        Toast.alert({ title: 'title example', message: 'message example' })
+	it('creates an alert toast', () => {
+		Toast.alert({ title: 'title example', message: 'message example' })
 
-        expect(Toast.notifications.length).toBe(1)
-        expect(Toast.notifications[0].title).toBe('title example')
-        expect(Toast.notifications[0].message).toBe('message example')
-        expect(Toast.notifications[0].type).toBe('error')
-    })
+		expect(Toast.notifications.length).toBe(1)
+		expect(Toast.notifications[0].title).toBe('title example')
+		expect(Toast.notifications[0].message).toBe('message example')
+		expect(Toast.notifications[0].type).toBe('error')
+	})
 
-    it('creates an info toast', () => {
-        Toast.info({ title: 'title example', message: 'message example' })
+	it('creates an info toast', () => {
+		Toast.info({ title: 'title example', message: 'message example' })
 
-        expect(Toast.notifications.length).toBe(1)
-        expect(Toast.notifications[0].title).toBe('title example')
-        expect(Toast.notifications[0].message).toBe('message example')
-        expect(Toast.notifications[0].type).toBe('info')
-    })
+		expect(Toast.notifications.length).toBe(1)
+		expect(Toast.notifications[0].title).toBe('title example')
+		expect(Toast.notifications[0].message).toBe('message example')
+		expect(Toast.notifications[0].type).toBe('info')
+	})
 })
